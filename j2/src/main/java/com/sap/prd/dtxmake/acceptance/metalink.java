cb226cef5e71f6b32f6ef2532dfaf2271c714f1c
I527375 <vibha.v@sap.com>
2023-03-25 12:06:35 +0530

    local files

---
+++ j2/src/main/java/com/sap/prd/dtxmake/acceptance/metalink.java
@@ -0,0 +1,10 @@
+package com.sap.prd.dtxmake.acceptance;
+import java.util.*;
+import java.lang.*;
+public class metalink
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!!");
+    }
+}
